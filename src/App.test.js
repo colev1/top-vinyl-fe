ba0a0fe91b65fcec4bda7d8c685283686d37ff50
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-06 15:50:28 -0700

    Setup testing suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import AlbumContainer from './AlbumContainer.js'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />)
+    
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render an album container', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.find(AlbumContainer).length).toEqual(1)
+  })
+
+  it('should change the state if the header is clicked', () => {
+    const wrapper = shallow(<App />)
+    const spy = jest.spyOn(wrapper.instance(), 'routeHome')
+    wrapper.instance().forceUpdate()
+
+    wrapper.find('.title').simulate('click')
+
+    expect(spy).toHaveBeenCalled()
+    expect(wrapper.state().displayTracks).toBe(false)
+  })
+})
