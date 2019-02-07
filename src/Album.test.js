c2b50fac0687afb02db6dd957a2bb2e4897242a9
Kaylee Bunce <kaybunce@hotmail.com>
2019-02-06 23:56:39 -0700

    Add test for component did mount

--- src/Album.test.js
+++ src/Album.test.js
@@ -5,7 +5,33 @@ import { shallow } from 'enzyme';
 import Album from './Album.js'
 
 describe('Album', () => {
+  let mockId
+  let mockToggle
+  let wrapper
+
+  beforeEach(() => {
+    mockId = 1
+    mockToggle = jest.fn()
+    wrapper = shallow(<Album albumId={ mockId } tiggleDisplay={ mockToggle } />)
+  })
+
   it('should match the snapshot', () => {
     
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a vinyl container', () => {
+    
+    expect(wrapper.find('.vinyl-container').length).toEqual(1)
+  })
+
+  it('should call fetchTrack if there is an albumid in props', () => {
+    const spyFetchTracks = jest.spyOn(wrapper.instance(), 'fetchTracks')
+    const spyFetchAlbum = jest.spyOn(wrapper.instance(), 'fetchAlbum')
+
+    wrapper.instance().componentDidMount()
+
+    expect(spyFetchTracks).toHaveBeenCalled()
+    expect(spyFetchAlbum).toHaveBeenCalled()
   })
 })
